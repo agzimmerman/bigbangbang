9ea48c3997e8c3332478da3da600bf036e301dfd
alexanderzimmerman <zimmerman@aices.rwth-aachen.de>
2017-05-13 18:22:40 +0200

    Added Body class

--- bigbangbang.py
+++ bigbangbang.py
@@ -4,7 +4,6 @@ import numpy as np
 
 # @todo Us vs. Them
 
-partcount = 3
 delay = 100
 dt = 1.
 G = 200.
@@ -16,7 +15,7 @@ center = (display_height/2., display_width/2.)
 screen = display.set_mode((int(display_height), int(display_width)))
 display.set_caption('Big Bang! Bang!')
 
-# @todo How to just change white pixels to a different color? Otherwise I need to make a new partpic that has a transparent background.
+circle = image.load('pics/circle.png')
 
 def color_surface(surface, red, green, blue):
     # https://gamedev.stackexchange.com/questions/26550/how-can-a-pygame-image-be-colored
@@ -25,53 +24,52 @@ def color_surface(surface, red, green, blue):
     arr[:,:,1] = green
     arr[:,:,2] = blue
     
-
-partpic = image.load('pics/circle.png')
-partpic = transform.scale(partpic, (30, 30))
-partpic.set_colorkey((0,0,0))
-partpic.convert_alpha()
-blue_partpic = partpic.copy()
-red_partpic = partpic.copy()
-color_surface(blue_partpic, 0, 0, 255)
-color_surface(red_partpic, 255, 0, 0)
-big_partpic = transform.scale2x(partpic)
-width, height = partpic.get_size()
-
-print width, height
-
-masses = [1., 1., 1.]
-starting_positions = [(center[0], center[1]), (center[0] + display_width/4., center[1]), (center[0] - display_width/4., center[1])]
-starting_velocities = [(0., 0.), (0., -1.), (0., 1.)]
-
-parts = []
-for ip in range(partcount):
-    parts.append(dict)
-    parts[ip] = {'mass': masses[ip], 'x': starting_positions[ip][0], 'y': starting_positions[ip][1], 'u': starting_velocities[ip][0], 'v': starting_velocities[ip][1], 'pic': partpic}
     
-parts[0]['pic'] = big_partpic
-parts[1]['pic'] = blue_partpic
-parts[2]['pic'] = red_partpic
-    
+class Body:
+
+    def __init__(self, mass=1, radius=1, position=(center[0], center[1]), velocity=(0,0), color=(255,255,255)):
+        
+        self.mass = mass
+        self.radius = radius
+        self.position = np.array(position)
+        self.velocity = np.array(velocity)
+        
+        self.pic = transform.scale(circle.copy(), (2*radius, 2*radius))
+        self.pic.set_colorkey((0,0,0))
+        self.pic.convert_alpha()
+        color_surface(self.pic, color[0], color[1], color[2])
+        
+    def draw(self):
+        screen.blit(self.pic, (int(round(self.position[0])) - self.radius, int(round(self.position[1])) - self.radius))
+        
+    def compute_force(self, other):
+        r = self.position - other.position
+        force = G*self.mass*other.mass*r/np.sqrt(r[0]**2 + r[1]**2)**3
+        return force
+        
+    def move(self, force):
+        self.position += self.velocity*dt
+        a = -force/self.mass
+        self.velocity += a*dt
+
+star = Body(mass=1, radius=50)
+us = Body(mass=1, radius=25, position=(center[0] + display_width/4., center[1]), velocity=(0., -1.), color=(0, 0, 255))
+them = Body(mass=1, radius=20, position=(center[0] - display_width/4., center[1]), velocity=(0., 1.), color=(255, 0, 0))
+
 done = False    
 while done == False:
 
     screen.fill(0)
     
-    screen.blit(parts[0]['pic'], (int(round(parts[0]['x']))  - width, int(round(parts[0]['y']))  - height))
+    star.draw()
     
-    for ip in range(1, partcount):
-        screen.blit(parts[ip]['pic'], (int(round(parts[ip]['x'])) - width/2, int(round(parts[ip]['y'])) - height/2))
-        parts[ip]['x'] += parts[ip]['u']*dt
-        parts[ip]['y'] += parts[ip]['v']*dt
-        rx = (parts[ip]['x'] - parts[0]['x'])
-        ry = (parts[ip]['y'] - parts[0]['y'])
-        r = np.sqrt(rx**2 + ry**2)
-        F = G*parts[0]['mass']*parts[ip]['mass']/r**2*np.array((rx, ry))/np.sqrt(rx**2 + ry**2)
-        a = -F/parts[ip]['mass']
-        parts[ip]['u'] += a[0]*dt
-        parts[ip]['v'] += a[1]*dt
+    for body in [us, them]:
+        body.draw()
+        force = body.compute_force(star)
+        body.move(force)
     
-    display.update()    
+    display.update()
+    
     time.delay(delay)
     
     for e in event.get():
@@ -79,4 +77,4 @@ while done == False:
             if e.key == K_ESCAPE:
                 done = True
 
-print "You lasted for", time.get_ticks()/1000, "seconds!"
+print "That took ", time.get_ticks()/1000, " seconds."
